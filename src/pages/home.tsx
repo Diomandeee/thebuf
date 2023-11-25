acd1f17d5c218d14110f8f1d6ccd2be100480aa2
Mohamed Diomande <gdiomande7907@gmail.com>
2023-11-25 10:27:38 -0500

    first committ

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,110 +1,21 @@
-import React from 'react'
-import Head from 'next/head'
+import React, { ReactElement } from 'react'
+import Home from '../components/Home'
 import Page from '@shared/Page'
-import Button from '@shared/atoms/Button'
 import { useRouter } from 'next/router'
 import { useMarketMetadata } from '@context/MarketMetadata'
 
-export default function IndexPage() {
-  const router = useRouter()
+export default function PageHome(): ReactElement {
   const { siteContent } = useMarketMetadata()
+  const router = useRouter()
 
-  const title = ''
-
-  const bookThemes = []
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <Page
-        title={siteContent?.siteTitle}
-        description={siteContent?.siteTagline}
-        uri={router.route}
-        headerCenter
-      >
-        <iframe
-          title="Meaning Full Power"
-          src="https://www.synergetic.life"
-          width="100%"
-          height="300"
-          frameBorder="0"
-          allowFullScreen
-          style={{ borderRadius: '15px' }} // Added rounded borders
-        ></iframe>
-
-        {bookThemes.map((theme) => (
-          <section
-            key={theme.title}
-            className="theme-section"
-            style={{ textAlign: 'center' }}
-          >
-            <h2>{theme.title}</h2>
-            <p>{theme.description}</p>
-          </section>
-        ))}
-
-        <div className="call-to-action">
-          <div
-            className="call-to-action"
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: '100px',
-              marginTop: '100px'
-            }}
-          >
-            <Button
-              style="primary"
-              to="https://www.amazon.com/Meaning-Full-Power-Harnessing-Potential/dp/B098HSP8Q9"
-            >
-              Explore the Book on Amazon
-            </Button>
-            {/* Add more buttons here with similar margin styles */}
-          </div>
-
-          <iframe
-            title="Meaning Full Power"
-            src="https://www.synergetic.life"
-            width="100%"
-            height="300"
-            frameBorder="0"
-            allowFullScreen
-            style={{ borderRadius: '15px' }} // Added rounded borders
-          ></iframe>
-        </div>
-        <div className="call-to-action">
-          <div
-            className="call-to-action"
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: '100px',
-              marginTop: '100px'
-            }}
-          >
-            <Button
-              style="primary"
-              to="https://www.amazon.com/Meaning-Full-Power-Harnessing-Potential/dp/B098HSP8Q9"
-            >
-              Explore the Book on Amazon
-            </Button>
-            {/* Add more buttons here with similar margin styles */}
-          </div>
-
-          <iframe
-            title="Meaning Full Power"
-            src="https://www.synergetic.life"
-            width="100%"
-            height="300"
-            frameBorder="0"
-            allowFullScreen
-            style={{ borderRadius: '15px' }} // Added rounded borders
-          ></iframe>
-        </div>
-      </Page>
-    </>
+    <Page
+      title={siteContent?.siteTitle}
+      description={siteContent?.siteTagline}
+      uri={router.route}
+      headerCenter
+    >
+      <Home />
+    </Page>
   )
 }
